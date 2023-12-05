eab18119ce4be96af68c9c0eb185755566e9fcbc
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2023-12-05 12:31:07 -0500

    Initial commit.

---
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -0,0 +1,24 @@
+package com.vaadin.starter.skeleton;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        final TextField fieldWithoutInitialValue = new TextField("No initial value");
+        final TextField fieldWithInitialValue = new TextField("Initial value", "Initial value", (String)null);
+
+        new Dialog(new VerticalLayout(fieldWithoutInitialValue, fieldWithInitialValue)).open();
+    }
+}
